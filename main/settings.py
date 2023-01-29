ee89b233aa0513a34ac64a514888d2ac8cfae033
dario.karmeinski <dario.karmeinski@gmail.com>
2023-01-29 18:43:22 +0100

    Adds templates

--- main/settings.py
+++ main/settings.py
@@ -121,11 +121,11 @@ STATIC_URL = 'static/'
 
 # Base static files or files from different directories:
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Absolute path to directory where collectstatic will collect static files for deployment.
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
